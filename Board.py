3dc655888be0a251df185b90e045cff1f97cd97b
strix990 <82136637+strix990@users.noreply.github.com>
2024-02-28 17:22:07 +0000

    Update Board.py

--- Board.py
+++ Board.py
@@ -24,7 +24,7 @@ def Red_moves(Game, red_human_player): #Computes Reds movement
     list_possible_moves = [False for _ in range(7)] #List of possible moves, for now its entirety is false
     if(red_human_player): #Checks if Red player is human or not, the negative case is not currently implemented
         print("Reds Turn")
-        for i in range(7): #Checks all columns to see if they are filled or not(in other words if a move is possible), if it is, print the corresponding number above it(didnt check the negative case yet)
+        for i in range(7): #Checks if all columns are filled (in other words if a move is possible), if it is, print the corresponding number above the column
             if(Return_Board.Grid[0][i] == 'X'):
                 print(i+1, end= " ")
                 list_possible_moves[i] = True #Also sets the possible moves list in its i possition to true
@@ -48,11 +48,11 @@ def Red_moves(Game, red_human_player): #Computes Reds movement
 def Blue_moves(Game, blue_human_player): #Computes Blues movement
     Return_Board = Board() 
     lista_temp = copy.deepcopy(Game.Grid)
-    setattr(Return_Board, 'Grid', lista_temp) #For the last three lines, because of python refence bullshittery, I had to make a deepcopy of the Board.Grid. Right now either the value is passed by reference or not doesn't make a difference but in order to no fuck up the code in the future I made it this way. Any other solutions are welcome since this looks ugly and must be pretty inneficient :3
-    list_possible_moves = [False for _ in range(7)] #List of possible moves, for now its entirety is false
-    if(blue_human_player): #Checks if blue player is human or not, the negative case is not currently implemented
+    setattr(Return_Board, 'Grid', lista_temp) #For the last three lines, because of python refence bullshittery, I had to make a deepcopy of the Board.Grid. Right now either the value is passed by reference or not doesn't matter but in order to no fuck up the code in the future I made it this way. Any other solutions are welcome since this looks ugly and must be pretty inneficient :3
+    list_possible_moves = [False for _ in range(7)] #List of possible moves
+    if(blue_human_player): #Checks if the blue player is human or not, the negative case is not currently implemented
         print("Blues Turn")
-        for i in range(7): #Checks all columns to see if they are filled or not(in other words if a move is possible), if it is, print the corresponding number above it(didnt check the negative case yet)
+        for i in range(7): #Checks if all columns are filled (in other words if a move is possible), if it is, print the corresponding number above the column
             if(Return_Board.Grid[0][i] == 'X'):
                 print(i+1, end= " ")
                 list_possible_moves[i] = True #Also sets the possible moves list in its i possition to true
@@ -76,7 +76,7 @@ def Blue_moves(Game, blue_human_player): #Computes Blues movement
 def Game_is_Over(test, color): #Checks if the Game is over
     for i in range(3):
         for j in range(4):
-            if(Check_Column(test, i, j, color)): # Here it checks the Column
+            if(Check_Column(test, i, j, color)): # Here it checks the Columns
                 print(color +" Wins")
                 test.print_grid()
                 return True
@@ -113,7 +113,7 @@ def main():
     blue_human_player = True
     print("Super duper ultra mega incredible IA project!") # Very important!
     test = Board()
-    while(True): #Used to play the game, first comes reds turn then if the game is not over comes blues turn and again if it's no over it loops back to reds turn
+    while(True): #Used for playing the game, first comes reds turn then if the game is not over comes blues turn and again if it's no over it loops back to reds turn
         test = Red_moves(test, red_human_player)
         if(Game_is_Over(test, 'R')):
             break
