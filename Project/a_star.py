e7dc3e05577ee1034decb5ed66ad73488129b768
strix990 <82136637+strix990@users.noreply.github.com>
2024-03-07 17:24:27 +0000

    Update a_star.py

--- Project/a_star.py
+++ Project/a_star.py
@@ -24,31 +24,50 @@ class Board_A: #The Board contains the games grid and a method to output the gri
 
 def Make_Move(Return_Board, index, color): #Computes the move, inputed by the index argument, by the player indentified by the color argument
     for i in range(5, -1, -1):
-        if(Return_Board.Grid[i][index-1] == 'X'):
-            Return_Board.Grid[i][index-1] = color
+        if(Return_Board.Grid[i][index] == 'X'):
+            Return_Board.Grid[i][index] = color
             return
     print("You should not be here")
 
+def Find_value(playerColor, value_temp):
+    position = -1
+    if(playerColor == 'R'):
+        greatest_value = -513
+        for i in range(7):
+            if(value_temp[i] > greatest_value):
+                greatest_value = value_temp[i]
+                position = i
+    elif(playerColor == 'B'):
+        greatest_value = 513
+        for i in range(7):
+            if(value_temp[i] < greatest_value):
+                greatest_value = value_temp[i]
+                position = i
+    print(position)
+    return position
+
 def A_Star(visited, playerColor, game, heuristic_value):
-    greatest_value = heuristic_value
-    move_position = 0
-    value_temp = 0
+    plusvalue = 0
+    value_temp_R = [-513]*7
+    value_temp_B = [513]*7
     for i in range(7):
         if (visited[i]):
             a_star_board = Board_A() 
             lista_temp = copy.deepcopy(game.Grid)
-            setattr(a_star_board, 'Grid', lista_temp) #For the last three lines, because of python refence bullshittery, I had to make a deepcopy of the Board.Grid. Right now either the value is passed by reference or not doesn't make a difference but in order to no fuck up the code in the future I made it this way. Any other solutions are welcome since this looks ugly and must be pretty inneficient :3
+            setattr(a_star_board, 'Grid', lista_temp) 
             Make_Move(a_star_board, i, playerColor)
-            value_temp = Total_Value(a_star_board)
-            print("Valor na (" + str(i) + " " + str(value_temp) + ")")
-            if(Is_Red(playerColor)):
-                 if(value_temp > greatest_value):
-                     greatest_value = value_temp
-                     move_position = i
-            else:
-                if(value_temp < greatest_value):
-                     greatest_value = value_temp
-                     move_position = i
-            value_temp = 0            
-    Make_Move(game, move_position, playerColor)    
-    return greatest_value           
+            if(playerColor == 'R'):
+                value_temp_R[i] = Total_Value(a_star_board)
+                print("Valor na (" + str(i) + " " + str(value_temp_R[i]) + ")")
+            elif(playerColor == 'B'):
+                value_temp_B[i] = Total_Value(a_star_board)
+                print("Valor na (" + str(i) + " " + str(value_temp_B[i]) + ")")
+    if(playerColor == 'R'):
+        move_position = Find_value(playerColor, value_temp_R)
+    elif(playerColor == 'B'):
+        move_position = Find_value(playerColor, value_temp_B)
+    Make_Move(game, move_position, playerColor)
+    if(playerColor == 'R'):  
+        return value_temp_R[move_position] + 16
+    else:
+        return value_temp_B[move_position] - 16     
